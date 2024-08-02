9f55cd1dffef92d3b9604cd01424376df92c2594
unknown <eric.coimbra@gmail.com>
2024-08-02 16:58:11 +0100

    removed unnecessary files

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
+import './index.css';
+
+import { MantineProvider } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,7 +12,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 );
 
